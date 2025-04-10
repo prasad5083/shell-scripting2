8fd5349d67dff4274dd161f96b9312cc3db6aebd
prasad5083 <deviprasad5083@gmail.com>
2025-04-10 19:22:28 +0530

    message is commited

--- 01.shabangnotation.sh
+++ 01.shabangnotation.sh
@@ -6,4 +6,8 @@ echo -e "line3\nLine4\tLine5"
 echo -e "\e[32m this is to practice the colour \e[0m"
 
 a=10
+b=20
+c=30
 echo value of the a i $a
+echo value of the b is $b
+echo value of the c is $c
