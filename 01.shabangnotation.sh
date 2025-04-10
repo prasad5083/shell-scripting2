cb1e36dd5981d55efc7099c6c232e8fd336bdf9d
prasad5083 <deviprasad5083@gmail.com>
2025-04-10 20:13:27 +0530

    message is commited

--- 01.shabangnotation.sh
+++ 01.shabangnotation.sh
@@ -5,8 +5,10 @@ echo line2
 echo -e "line3\nLine4\tLine5"
 echo -e "\e[32m this is to practice the colour \e[0m"
 
-a=
+a=10
 b=20
 c=30
-echo value of the a i $a
+echo value of the a is $a
+date=date =%F
+echo todays date is ${date}
 
