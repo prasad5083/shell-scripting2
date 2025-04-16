75fec4b1ccc39c1d35d5ecdabeed95d8449320e0
prasad5083 <deviprasad5083@gmail.com>
2025-04-16 19:28:22 +0530

    message is commited

--- is-else.sh
+++ is-else.sh
@@ -2,7 +2,7 @@ action=$1
 
 if [ "$action" == "start" ] ; then
     echo "rabbit MQ is started"
-elif [ "$action" == "stop"] ; then
+elif [ "$action" == "stop" ] ; then
     echo " rabbit MQ is stoped"
 elif [ "$action" == "restart" ] ; then
     echo " rabbit MQ is restarted"        
