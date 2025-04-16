b20722770ff0839527e3acb6d1b253c8c6de8ff3
prasad5083 <deviprasad5083@gmail.com>
2025-04-16 18:26:13 +0530

    message is commited

--- 02.cash.sh
+++ 02.cash.sh
@@ -2,14 +2,18 @@ action=$1
 case $action in 
     start)
     echo -e "\e[32m rabbit MQ started \e[0m"
+    exit 0
     ;;
     stop)
     echo "rabbit MQ stopped"
+    exit 1
     ;;
     restart)
     echo "rabbit MQ restarted"
+    exit 2
     ;;
     *)
     echo " onlt aviable options are start, stop and restart"
+    exit 4
     ;;
 esac    
