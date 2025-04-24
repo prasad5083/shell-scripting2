6becb5d0c0838d1c8ac31d2515578dacb8c92152
prasad5083 <deviprasad5083@gmail.com>
2025-04-24 18:39:41 +0530

    message is commited

--- roboshop/components/frontendd.sh
+++ roboshop/components/frontendd.sh
@@ -40,12 +40,14 @@ rm -rf ${component}-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
 stat $?
 
-echo " configuring firewall "
-firewall-cmd --add-port=22/tcp --permanent &>> $logfile
-firewall-cmd --reload &>> $logfile
-stat $?
+#echo " configuring firewall "
+#firewall-cmd --add-port=22/tcp --permanent &>> $logfile
+#firewall-cmd --reload &>> $logfile
+#stat $?
+
 
 echo " starting the ${component} service "
 systemctl enable nginx &>> $logfile
 systemctl restart nginx &>> $logfile
+systemctl start nginx &>> $logfile
 stat $?
