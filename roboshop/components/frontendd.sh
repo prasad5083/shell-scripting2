c844c903028af1a737e347bfeca0578d4a8e4c04
prasad5083 <deviprasad5083@gmail.com>
2025-04-24 18:49:51 +0530

    message is commited

--- roboshop/components/frontendd.sh
+++ roboshop/components/frontendd.sh
@@ -2,9 +2,8 @@
 
 component=frontend
 ID=$(id -u)
-
 if [ $ID -ne 0 ] ; then
-  echo -e "\e[31m this script is only execurted by only previlaged user \e[0m"
+  echo -e "\e[31m this script is only executed by privileged user \e[0m"
   exit 1
 fi 
 
@@ -19,33 +18,28 @@ stat() {
 
 logfile="/tmp/${component}.log"
 
-echo " install nginx : "
-yum install nginx -y &>> $logfile
+echo "install nginx : "
+yum install nginx unzip -y &>> $logfile
 stat $? 
 
-echo "downoding the front end component"
+echo "downloading the front end component"
 curl -s -L -o /tmp/${component}.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
 stat $?
 
-echo -n -e "  performing clean up "
+echo -n "performing clean up "
 cd /usr/share/nginx/html  
 rm -rf * &>> $logfile
 stat $?
 
-echo -n " extracting ${component} component : "
+echo -n "extracting ${component} component : "
 unzip /tmp/${component}.zip &>> $logfile
 mv ${component}-main/* . 
-mv ${component}-main/static/* . &>> $logfile
+mv static/* . 
 rm -rf ${component}-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
 stat $?
 
-echo " configuring firewall "
-firewall-cmd --add-port=80/tcp --permanent &>> $logfile
-firewall-cmd --reload &>> $logfile
-stat $?
-
-echo " starting the ${component} service "
+echo "starting the ${component} service"
 systemctl enable nginx &>> $logfile
-systemctl restart nginx &>> $logfile
+systemctl start nginx &>> $logfile
 stat $?
