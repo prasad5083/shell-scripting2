ebec465ab8351e58006c046605b0e4ea54706ee1
prasad5083 <deviprasad5083@gmail.com>
2025-04-24 19:12:20 +0530

    message is commited

--- roboshop/components/frontendd.sh
+++ roboshop/components/frontendd.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 component=frontend
+logfile="/tmp/${component}.log"
 ID=$(id -u)
 
-# Check if the script is executed by a privileged user
-if [ $ID -ne 0 ] ; then
-  echo -e "\e[31mThis script must be executed by a privileged user\e[0m"
+# Check for root privileges
+if [ $ID -ne 0 ]; then
+  echo -e "\e[31mThis script must be run as root.\e[0m"
   exit 1
-fi 
+fi
 
-# Function to check status of commands
+# Function to print status
 stat() {
-  if [ $1 -eq 0 ] ; then
+  if [ $1 -eq 0 ]; then
     echo -e "\e[32mSUCCESS\e[0m"
   else
     echo -e "\e[31mFAILURE\e[0m"
@@ -19,35 +20,45 @@ stat() {
   fi
 }
 
-logfile="/tmp/${component}.log"
-
-echo "Installing Nginx: "
+echo "Installing Nginx:"
 yum install nginx -y &>> $logfile
-stat $? 
+stat $?
 
-echo "Downloading the frontend component..."
+echo "Downloading frontend component:"
 curl -s -L -o /tmp/${component}.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
 stat $?
 
-echo -n "Performing cleanup..."
-cd /usr/share/nginx/html || exit 1
-rm -rf * &>> $logfile
+echo "Cleaning up old HTML content:"
+rm -rf /usr/share/nginx/html/* &>> $logfile
 stat $?
 
-echo -n "Extracting ${component} component: "
-unzip /tmp/${component}.zip &>> $logfile
-mv ${component}-main/* . 
-mv ${component}-main/static/* . &>> $logfile
-rm -rf ${component}-main README.md
-mv localhost.conf /etc/nginx/default.d/roboshop.conf
+echo "Extracting frontend content:"
+unzip /tmp/${component}.zip -d /tmp &>> $logfile
+mv /tmp/frontend-main/* /usr/share/nginx/html/ &>> $logfile
 stat $?
 
-echo "Configuring firewall..."
+echo "Setting correct file permissions:"
+chown -R nginx:nginx /usr/share/nginx/html
+chmod -R 755 /usr/share/nginx/html
+stat $?
+
+echo "Creating Nginx config:"
+cat <<EOF > /etc/nginx/default.d/roboshop.conf
+location / {
+    root   /usr/share/nginx/html;
+    index  index.html index.htm;
+}
+EOF
+stat $?
+
+echo "Enabling and restarting Nginx:"
+systemctl enable nginx &>> $logfile
+systemctl restart nginx &>> $logfile
+stat $?
+
+echo "Configuring firewall:"
 firewall-cmd --add-port=80/tcp --permanent &>> $logfile
 firewall-cmd --reload &>> $logfile
 stat $?
 
-echo "Starting the ${component} service..."
-systemctl enable nginx &>> $logfile
-systemctl restart nginx &>> $logfile
-stat $?
+echo -e "\n\e[32mFrontend setup complete! Visit http://<EC2-IP> to verify.\e[0m"
