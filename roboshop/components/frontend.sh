7b160a0d30fc4e304d53955d53018757fe61211e
prasad5083 <deviprasad5083@gmail.com>
2025-04-24 15:34:16 +0530

    message is commited

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+ID=$(id -u)
+if[ID -ne 0] ; then
+ echo -e "\e[31m this script is only execurted by only previlaged user \e[0m"
+ exit 1
+fi 
 echo " install nginx : "
 
 yum install nginx -y
@@ -6,4 +12,23 @@ yum install nginx -y
 # yum install nginx -y
 # systemctl enable nginx
 # systemctl start nginx
+
+
+
+#Let's download the HTDOCS content and deploy it under the Nginx path.
+
+# curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+
+#Deploy in Nginx Default Location.
+
+# cd /usr/share/nginx/html
+# rm -rf *
+# unzip /tmp/frontend.zip
+# mv frontend-main/* .
+# mv static/* .
+# rm -rf frontend-main README.md
+# mv localhost.conf /etc/nginx/default.d/roboshop.conf
+
+
+
 
